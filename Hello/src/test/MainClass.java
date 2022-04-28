0fa2c7de8a9affef71534832f2fd0f5b5588ccf7
joeyoonam <koko6613@hanmail.net>
2022-04-28 17:01:42 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,17 @@
+package test;
+
+public class MainClass {
+	public static void main(String[] args) {
+		//run(실행) 했을 때 실행의 흐름이 시작되는 main 메소드 
+		System.out.println("Hello, World");
+		
+		String str="kimgura";
+		
+		int num1=10;
+		
+		boolean isRun=true;
+		
+		
+	}
+	
+}
